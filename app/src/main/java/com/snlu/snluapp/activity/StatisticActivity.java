c65f27496ae5e8a978efd89da9af22910e714840
Honest-Hong <htj889@gmail.com>
2017-05-08 12:49:43 +0900

    통계 액티비티 주석 제거

--- app/src/main/java/com/snlu/snluapp/activity/StatisticActivity.java
+++ app/src/main/java/com/snlu/snluapp/activity/StatisticActivity.java
@@ -64,118 +64,6 @@ public class StatisticActivity extends AppCompatActivity {
 
         requestStatistic();
     }
-  /*          super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_statistc);
-        Log.d(TAG,"onCreate starting to create chart");
-
-        document = new DocumentItem();
-        document.setNumber(getIntent().getStringExtra("documentNumber"));
-
-        pieChart=(PieChart)findViewById(R.id.statistic_pie_chart);
-        pieChart.setHoleRadius(20f);
-        pieChart.setCenterText("단어통계");
-        pieChart.setCenterTextSize(10);
-        pieChart.setDrawEntryLabels(true);
-        pieChart.setUsePercentValues(true);
-        pieChart.setRotationEnabled(true);
-
-        requestStatistic();
-    }
-
-    private void requestStatistic(){
-        JSONObject json = new JSONObject();
-        try{
-            json.put("documentNumber",document.getNumber());
-            SNLUVolley.getInstance(this).post("analyze", json, new SNLUVolley.OnResponseListener() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    Log.v("TAG", response.toString());
-                    try {
-                        int result = response.getInt("result");
-                        if(result==0) {
-                            String str = response.getString("data");
-                            JSONArray array = new JSONArray(str);
-
-                            int cnt = array.length();
-                            yData = new float[cnt];
-                            xData = new String[cnt];
-                            for(int i=0; i<cnt ;i++){
-                                yData[i]=array.getJSONObject(i).getInt("count");
-                            }
-                            for(int i =0; i<cnt; i++){
-                                xData[i] = array.getJSONObject(i).getString("name");
-                            }
-                            addDataSet();
-                        } else {
-                            Log.v("TAG", "error");
-                        }
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-    private void addDataSet() {
-        Log.d(TAG,"addDataSet started");
-
-        Legend legend = pieChart.getLegend();
-
-        ArrayList<PieEntry> yEntry = new ArrayList<PieEntry>();
-        for(int i = 0; i<yData.length;i++)
-            yEntry.add(new PieEntry(yData[i], xData[i]));
-
-        // drawPieChart();
-        PieDataSet dataSet = new PieDataSet(yEntry,"");
-        dataSet.setSliceSpace(3);
-        dataSet.setSelectionShift(5);
-
-        legend.setEnabled(true);
-        PieData data = new PieData(dataSet);
-
-        //add colors to dataset
-        ArrayList<Integer> colors = new ArrayList<Integer>();
-        for (int c : ColorTemplate.VORDIPLOM_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.JOYFUL_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.COLORFUL_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.LIBERTY_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.PASTEL_COLORS)
-            colors.add(c);
-
-        colors.add(ColorTemplate.getHoloBlue());
-
-        dataSet.setColors(colors);
-
-        data.setValueFormatter(new PercentFormatter());
-        data.setValueTextSize(11f);
-        data.setValueTextColor(Color.WHITE);
-        pieChart.setData(data);
-
-        pieChart.invalidate();
-
-        pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
-
-        //add legend to chart
-        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        legend.setForm(Legend.LegendForm.CIRCLE);
-        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
-        legend.setOrientation(Legend.LegendOrientation.VERTICAL);
-        legend.setDrawInside(false);
-        legend.setXEntrySpace(7);
-        legend.setYEntrySpace(5);
-        legend.setYOffset(0f);
-    }*/
 
     private void requestStatistic() {
         JSONObject json = new JSONObject();
@@ -259,8 +147,7 @@ public class StatisticActivity extends AppCompatActivity {
         }
         barDataSet = new BarDataSet(yEntry, "단어");
         barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        barDataSet.setStackLabels(xData);
-
+//        barDataSet.setStackLabels(xData);
         barData = new BarData(barDataSet);
         barData.setBarWidth(0.9f);
 
